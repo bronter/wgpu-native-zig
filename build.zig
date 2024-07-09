957a8bc4657ea282b9de09703da15816d17f797b
Gabriel Charleboix <gcharleboix@gmail.com>
2024-07-08 22:45:12 -0500

    Clean up build file a bit

--- build.zig
+++ build.zig
@@ -15,15 +15,6 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // const lib = b.addStaticLibrary(.{
-    //     .name = "wgpu-native-zig",
-    //     // In this case the main source file is merely a path, however, in more
-    //     // complicated build scripts, this could be a generated file.
-    //     .root_source_file = b.path("src/root.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
     const mod = b.addModule("wgpu", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
@@ -35,47 +26,6 @@ pub fn build(b: *std.Build) void {
 
     mod.addIncludePath(wgpu_dep.path(""));
     mod.addObjectFile(wgpu_dep.path("libwgpu_native.a"));
-    // lib.linkLibCpp();
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    // b.installArtifact(lib);
-
-    // const exe = b.addExecutable(.{
-    //     .name = "wgpu-native-zig",
-    //     .root_source_file = b.path("src/main.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    // b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    // const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    // run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    // if (b.args) |args| {
-    //     run_cmd.addArgs(args);
-    // }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    // const run_step = b.step("run", "Run the app");
-    // run_step.dependOn(&run_cmd.step);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
@@ -87,24 +37,10 @@ pub fn build(b: *std.Build) void {
     });
     compute_test.root_module.addImport("wgpu", mod);
 
-    // lib_unit_tests.addIncludePath(wgpu_dep.path(""));
-    // lib_unit_tests.addObjectFile(wgpu_dep.path("libwgpu_native.a"));
-    // lib_unit_tests.linkLibCpp();
-
     const run_compute_test = b.addRunArtifact(compute_test);
 
-    // const exe_unit_tests = b.addTest(.{
-    //     .root_source_file = b.path("src/main.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-
-    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
+    // This exposes a `test` step to the `zig build --help` menu,
+    // providing a way for the user to request running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_compute_test.step);
-    // test_step.dependOn(&run_exe_unit_tests.step);
 }
