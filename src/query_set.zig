26db9ecd61673d73645786cda06b9498320f64e3
Gabriel Charleboix <gcharleboix@gmail.com>
2024-07-20 17:46:45 -0500

    Fill in methods of QuerySet

--- src/query_set.zig
+++ src/query_set.zig
@@ -12,6 +12,39 @@ pub const QuerySetDescriptor = extern struct {
     count: u32,
 };
 
+pub const QuerySetProcs = struct {
+    pub const Destroy = *const fn(*QuerySet) callconv(.C) void;
+    pub const GetCount = *const fn(*QuerySet) callconv(.C) u32;
+    pub const GetType = *const fn(*QuerySet) callconv(.C) QueryType;
+    pub const SetLabel = *const fn(*QuerySet, ?[*:0]const u8) callconv(.C) void;
+    pub const Reference = *const fn(*QuerySet) callconv(.C) void;
+    pub const Release = *const fn(*QuerySet) callconv(.C) void;
+};
+
+extern fn wgpuQuerySetDestroy(query_set: *QuerySet) callconv(.C) void;
+extern fn wgpuQuerySetGetCount(query_set: *QuerySet) callconv(.C) u32;
+extern fn wgpuQuerySetGetType(query_set: *QuerySet) callconv(.C) QueryType;
+extern fn wgpuQuerySetSetLabel(query_set: *QuerySet, label: ?[*:0]const u8) callconv(.C) void;
+extern fn wgpuQuerySetReference(query_set: *QuerySet) callconv(.C) void;
+extern fn wgpuQuerySetRelease(query_set: *QuerySet) callconv(.C) void;
+
 pub const QuerySet = opaque {
-    // TODO: fill in methods
+    pub inline fn destroy(self: *QuerySet) void {
+        wgpuQuerySetDestroy(self);
+    }
+    pub inline fn getCount(self: *QuerySet) u32 {
+        return wgpuQuerySetGetCount(self);
+    }
+    pub inline fn getType(self: *QuerySet) QueryType {
+        return wgpuQuerySetGetType(self);
+    }
+    pub inline fn setLabel(self: *QuerySet, label: ?[*:0]const u8) void {
+        wgpuQuerySetSetLabel(self, label);
+    }
+    pub inline fn reference(self: *QuerySet) void {
+        wgpuQuerySetReference(self);
+    }
+    pub inline fn release(self: *QuerySet) void {
+        wgpuQuerySetRelease(self);
+    }
 };
